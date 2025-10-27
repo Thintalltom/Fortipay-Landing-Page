17173b4ce015e17d4be1fc35e64ca95e3d7978c7
Thintalltom <adeyanjutomide@gmail.com>
2025-10-27 09:20:02 +0100

    added some navigation changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,22 @@
 
 import FortPay from '../../assets/BlueForti.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+    const location = useLocation()
+    
+    const isActive = (path: string) => location.pathname === path
+    
     return (
         <nav className="relative z-10 p-4 flex justify-center items-center">
             <div className='flex gap-[40px] md:gap-[60px] lg:gap-[80px] w-full max-w-[600px] md:max-w-[800px] lg:max-w-[1000px] px-[12px] md:px-[14px] lg:px-[16px] py-[20px] md:py-[22px] lg:py-[24px] rounded-[50px] justify-center items-center h-[64px] md:h-[68px] lg:h-[72px] bg-white shadow-[0_4px_20px_rgba(0,0,0,0.1)]'>
                 <img src={FortPay} />
-                <div className='flex gap-[40px] font-[Lato] text-[#848484] text-[14px]'>
-                    <Link to='/'>Home</Link>
-                    <Link to='/solution'>Solution</Link>
-                    <Link to='/about'>About us</Link>
-                    <Link to='/blog'>Blog</Link>
-                    <Link to='/contact'>Contact Us</Link>
+                <div className='flex gap-[40px] font-[Lato] text-[14px]'>
+                    <Link to='/' className={isActive('/') ? 'text-[#03377D]' : 'text-[#848484]'}>Home</Link>
+                    <Link to='/solution' className={isActive('/solution') ? 'text-[#03377D]' : 'text-[#848484]'}>Solution</Link>
+                    <Link to='/about' className={isActive('/about') ? 'text-[#03377D]' : 'text-[#848484]'}>About us</Link>
+                    <Link to='/blog' className={isActive('/blog') ? 'text-[#03377D]' : 'text-[#848484]'}>Blog</Link>
+                    <Link to='/contact' className={isActive('/contact') ? 'text-[#03377D]' : 'text-[#848484]'}>Contact Us</Link>
                 </div>
                 <div className='flex gap-[20px]'>
                     <button className='border-[#03377D] border-[1px] rounded-[40px] text-[14px] font-[Lato] font-semibold py-[10px] px-[16px] text-[#03377D]'>Join Waitlist</button>
