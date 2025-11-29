40f9cfeaeac97e7451277cd273860dbf5e50e5a6
Tomide Adeyanju <tadeyanju@infinion.co>
2025-11-29 16:43:31 +0100

    adde few changes

--- src/Pages/About/ImageSection.tsx
+++ src/Pages/About/ImageSection.tsx
@@ -2,7 +2,7 @@ const ImageSection = () => {
   return (
      <div className='p-[80px]  h-fit'>
         <div className='relative justify-center items-center flex mt-[-200px] h-fit '>
-            <img src='https://images.ctfassets.net/t38lsvt2xzqb/2bkDcJetQDLIwYSyauMZ77/c1797dd5c35d0768ede8caadf7334283/padlock.png' alt='Lock Image' className='h-[714px] flex-1 w-[1280px]' />
+            <img src='https://images.ctfassets.net/t38lsvt2xzqb/5wlVPzpbihQYzYYGpRU1LD/f6190a809f6df6a8dec06d5e452410d9/StrengthImage.png?h=250' alt='Lock Image' className='h-[714px] flex-1 w-[1280px] rounded-t-lg' />
             <div className='absolute inset-0 bg-gradient-to-b from-transparent to-white opacity-100'></div>
             
         </div>
